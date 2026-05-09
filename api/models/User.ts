a216963c495c94dbcc008a3ffebeb8cd1a29e4db
rangerx02 <gean.9720@gmail.com>
2026-05-09 17:28:54 -0500

    Adjustment for product creation by admin and connection with Instagram

--- api/models/User.ts
+++ api/models/User.ts
@@ -1,9 +1,13 @@
+import admin from 'firebase-admin';
+
 export interface User {
   uid: string;
   correo: string;
   nombre: string;
+  apellidos?: string;
   rol: 'cliente' | 'admin';
   telefono?: number;
   direccion?: string;
-  fechaRegistro?: Date;
+  birthdate?: admin.firestore.Timestamp;
+  fechaRegistro?: admin.firestore.Timestamp;
 }
